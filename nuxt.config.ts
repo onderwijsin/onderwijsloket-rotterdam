6cd7ca2a7c2be950b71de43fbb0e41e08c89dac2
remihuigen <remi@bureaubox.nl>
2024-12-30 12:44:20 +0100

    add map an dmotion packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
   devtools: { enabled: isDev && useDevtools },
 
   extends: [
-    [`github:onderwijsin/onderwijsloket-layer-core#v0.7.2`, { auth: process.env.GIGET_AUTH, install: true }],
+    [`github:onderwijsin/onderwijsloket-layer-core#v0.7.3`, { auth: process.env.GIGET_AUTH, install: true }],
     // [`github:onderwijsin/onderwijsloket-layer-navigator#v${navigatorLayerVersion}`, { auth: process.env.GIGET_AUTH, install: true }],
   ],
 
