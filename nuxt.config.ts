c737c1fe07fe37018e585c624576728638c377b6
remihuigen <remi@bureaubox.nl>
2025-01-09 18:07:44 +0100

    upgrade core 0.8.6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
   devtools: { enabled: isDev && useDevtools },
 
   extends: [
-    [`github:onderwijsin/onderwijsloket-layer-core#v0.8.5`, { auth: process.env.GIGET_AUTH, install: true }],
+    [`github:onderwijsin/onderwijsloket-layer-core#v0.8.6`, { auth: process.env.GIGET_AUTH, install: true }],
     // [`github:onderwijsin/onderwijsloket-layer-navigator#v${navigatorLayerVersion}`, { auth: process.env.GIGET_AUTH, install: true }],
   ],
 
